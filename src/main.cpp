7e4085779224ba17a50e483f4bbada7564215682
carlosahs <tacahs12@gmail.com>
2021-09-20 07:13:28 -0500

    Test sorting of chemical elements

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,42 @@
 #include <iostream>
+#include <vector>
 
 #include "ChemicalElement.h"
+#include "sorts.h"
+
+bool test_sort(std::vector<ChemicalElement> sorted, 
+    std::vector<ChemicalElement> expected
+)
+{
+    for (int i = 0; i < sorted.size(); i++)
+    {
+        if (sorted[i] != expected[i])
+        {
+            return false;
+        }
+    }
+    return true;
+}
 
 int main()
 {
+    ChemicalElement hydrogen = ChemicalElement();
+    ChemicalElement helium = ChemicalElement("Hellium", "He", 2, 4.00260);
+    ChemicalElement oxygen = ChemicalElement("Oxygen", "O", 8, 15.999);
+    ChemicalElement magnesium = ChemicalElement("Magnesium", "Mg", 12, 24.305);
+    ChemicalElement molybdenum = ChemicalElement("Molybdenum", "Mo", 42, 95.95);
+    ChemicalElement xenon = ChemicalElement("Xenon", "Xe", 54, 131.29);
+
+    std::vector<ChemicalElement> elements_unsorted { xenon, 
+        helium, molybdenum, hydrogen, magnesium, oxygen };
+    std::vector<ChemicalElement> elements_sorted { hydrogen,
+        helium, oxygen, magnesium, molybdenum, xenon };
+
+    Sorts::merge_sort(elements_unsorted);
+    std::cout << "Sort was successful? ";
+    std::cout << std::boolalpha 
+        << test_sort(elements_unsorted, elements_sorted)
+        << "\n";
+
     return 0;
 }
